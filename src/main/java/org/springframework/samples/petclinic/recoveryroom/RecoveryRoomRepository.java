c2aa3c714ed1b9a48516d3310c441d7e2f1d8ac0
Paula <paulaferreirajimenez@gmail.com>
2022-01-14 17:15:26 +0100

    Control Check pauferjim

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,11 +3,27 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
-    List<RecoveryRoom> findAll();
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
-    Optional<RecoveryRoom> findById(int id);
-    RecoveryRoom save(RecoveryRoom p);
-    //RecoveryRoomType getRecoveryRoomType(String name);
-    //List<RecoveryRoom> findBySizeMoreThan(double size);
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer>{
+    
+	@Query("SELECT r from RecoveryRoom r")
+	List<RecoveryRoom> findAll();
+
+	@Query("SELECT r from RecoveryRoomType r")
+	List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    
+	Optional<RecoveryRoom> findById(int id);
+    
+	RecoveryRoom save(RecoveryRoom p);
+    
+    @Query("SELECT r from RecoveryRoomType r WHERE r.name=:name")
+    RecoveryRoomType getRecoveryRoomType(@Param("name") String name);
+    
+    @Query("SELECT r from RecoveryRoom r WHERE r.size>:size")
+    List<RecoveryRoom> findBySizeMoreThan( @Param("size")double size);
 }
